a8f048f8e4bfd146a0b2634676214638b6ad84d8
-inahlainepacatang <inahlainep@gmail.com>
2025-05-06 18:50:04 +0800

    second commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 @app.route('/')
@@ -10,6 +11,7 @@ def index():
         "Semi_Final Exam"
     )
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
